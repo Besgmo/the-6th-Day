b664016b1e7d63cb4902b1742cc34e3a73d6fe4d
Igor <besgmo@gmail.com>
2017-09-07 14:47:09 +0300

    Initial Commit

---
+++ the 6th Day/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  the 6th Day
+//
+//  Created by Igor on 07.09.17.
+//  Copyright © 2017 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
