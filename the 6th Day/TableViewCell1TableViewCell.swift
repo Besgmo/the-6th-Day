4389a0674cc49a515fa96c87a7bf1cc08e241a57
Igor <besgmo@gmail.com>
2017-09-07 15:39:41 +0300

    Add 1st page

---
+++ the 6th Day/TableViewCell1TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell1TableViewCell.swift
+//  the 6th Day
+//
+//  Created by Igor on 07.09.17.
+//  Copyright © 2017 Igor. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
